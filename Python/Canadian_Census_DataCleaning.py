863116b0b7ccc002b5b7e8c7479098df5d0f3412
renacin <renacin.matadeen@ryerson.ca>
2020-04-07 11:10:13 -0400

    Need To Find Better Data Correction Methodology

--- Python/Canadian_Census_DataCleaning.py
+++ Python/Canadian_Census_DataCleaning.py
@@ -5,18 +5,20 @@
 # ----------------------------------------------------------------------------------------------------------------------
 import csv
 import pandas as pd
-from os import listdir
+import numpy as np
+import time
 # ----------------------------------------------------------------------------------------------------------------------
-
 
 """
 Open The CSV, and Pull Information You Need
 """
 def parse_toronto_csv(in_path, out_path):
-
     temp_df = pd.DataFrame()
+
+    # [1.] File Is Too Large To Hold In Memory, Read In 1'000'000 Line Chunks
     chunksize = 10 ** 6
 
+    # [2.] Filter Geo Polygons Based On GEO_NAME, Append To New DF
     counter = 1
     for chunk in pd.read_csv(in_path, chunksize=chunksize):
 
@@ -31,6 +33,7 @@ def parse_toronto_csv(in_path, out_path):
         print("Progress: Chunk {}/47".format(counter)) # Write To A CSV, This Takes Up Too Much Memory
         counter += 1
 
+    # [3.] Write To CSV, Don't Repeat Intensive Processes
     temp_df.to_csv(out_path, index=False)
 
 
@@ -59,26 +62,23 @@ def clean_data(in_path, out_path):
 Reorganize Data So Accesible By GIS
 """
 def reorg_df(in_path, out_path):
-
-    # [1.] Read Data Parsed & Cleaned Data
     tc_data = pd.read_csv(in_path)
 
-
-    # [2.] Grab Names For Columns
+    # [1.] Grab Names For Columns
     unq_census_fields = list(tc_data["Column"][0:2247])
 
     columns_list = []
     for idx, field in enumerate(unq_census_fields):
-        toreplace = [" ", "_", ",", "x", "-"]
+        toreplace = [" ", "_", ",", "-", "..."]
         for char in toreplace:
             field = field.replace(char, "")
 
         columns_list.append(str(idx + 1) + "_" + field)
 
-    # [3.] Get Unique Values DA_IDs, And Create A DA_ID Column
+    # [2.] Get Unique Values DA_IDs, And Create A DA_ID Column
     unq_da = list(tc_data["DA_ID"].unique())
 
-    # [4.] Create a List of Lists To Store Data For Each Row
+    # [3.] Create a List of Lists To Store Data For Each Row
     rows_list = []
     start_idx = 0
     end_idx = 2247
@@ -93,12 +93,39 @@ def reorg_df(in_path, out_path):
 
         rows_list.append(list(tc_data["Count"][start_idx:end_idx]))
 
-    # [5.] Create a new DF with data
+    # [4.] Create a new DF with data
     cleaned_df = pd.DataFrame(rows_list, columns = columns_list)
 
-    # [6.] First Row Needs DA_IDs
+    # [5.] First Row Needs DA_IDs
     cleaned_df.insert(loc=0, column="DA_ID", value=unq_da)
     cleaned_df.to_csv(out_path, index=False)
+
+
+"""
+Fill In Missing Data | Which Methodology Works Best?
+1). Mean, Median, or Mode | Median Best Of Three, But Still Not As Accurate
+2). Nearest neighbour Fill?
+"""
+def fill_df(in_path, out_path):
+    df = pd.read_csv(in_path, low_memory=False)
+
+    # [1.] Convert All Non-Numeric Values To Nan
+    df = df.convert_objects(convert_numeric=True)
+    df = df.replace('...', np.NaN)
+
+    # [2.] Loop Through Each Column And Fill Nan With Column Average
+    column_names = list(df.columns.values)
+
+    for field_name in column_names:
+
+        if field_name != "DA_ID":
+            column_average = round(df[field_name].median(), 2)
+            df[field_name] = df[field_name].fillna(column_average)
+            del column_average
+
+    # [3.] Export Data
+    df.to_csv(out_path, index=False)
+
 
 # ----------------------------------------------------------------------------------------------------------------------
 if __name__ == "__main__":
@@ -107,6 +134,9 @@ if __name__ == "__main__":
     #
     # clean_data(r"C:\Users\renac\Documents\Programming\Python\CanadianCensus_DataCleaning\Data\Step_1_CensusData.csv",
     #            r"C:\Users\renac\Documents\Programming\Python\CanadianCensus_DataCleaning\Data\Step_2_CensusData.csv")
-
-    reorg_df(r"C:\Users\renac\Documents\Programming\Python\CanadianCensus_DataCleaning\Data\Step_2_CensusData.csv",
-             r"C:\Users\renac\Documents\Programming\Python\CanadianCensus_DataCleaning\Data\TorontoCensusData_DA.csv")
+    #
+    # reorg_df(r"C:\Users\renac\Documents\Programming\Python\CanadianCensus_DataCleaning\Data\Step_2_CensusData.csv",
+    #          r"C:\Users\renac\Documents\Programming\Python\CanadianCensus_DataCleaning\Data\Step_3_CensusData.csv")
+    
+    fill_df(r"C:\Users\renac\Documents\Programming\Python\CanadianCensus_DataCleaning\Data\Step_3_CensusData.csv",
+             r"C:\Users\renac\Documents\Programming\Python\CanadianCensus_DataCleaning\Data\Step_4_CensusData.csv")
