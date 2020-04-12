c8b1079f5b31facbca7b85e366d2e38e0a662f08
renacin <renacin.matadeen@ryerson.ca>
2020-04-12 13:01:16 -0400

    Filling Nan Values With Mean Is Detrimental To Any Prediction Model, Skip Nan Values When Training A Model

--- Research/RandomRoundingDefeat.py
+++ Research/RandomRoundingDefeat.py
@@ -18,6 +18,7 @@ Notes:
     + From this experiment we can surmise that randon rounding (with cosideration to rounding bases of 5, or 10) has a
         large influence on smaller census observations
     + Use Numpy Arrays. Much Faster And Designed For This Kind Of Work
+    + Don't Fill Nan With Mean, Screws Up Prediction Model, Just Omit
 
 """
 # ----------------------------------------------------------------------------------------------------------------------
@@ -25,34 +26,54 @@ Notes:
 """
 Create Data For Random Rounding Research
     + Create a dataset, introduce an acceptable amount of noise into both variables
+    + If variables are small remove and replace with NaN (Simulate Small Area Confidentiality)
 """
-def create_data(num_points):
+def create_data(num_points, lower_cutout):
 
     # [1.] Create X & Y Variables
-    x = np.random.uniform(low=50, high=1000, size=(num_points,))
+    x = np.random.uniform(low=0, high=1000, size=(num_points,))
     y_1 = (6.5 * x) + np.random.uniform(low=0, high=100, size=(1,))
 
     # [2.] Randomly Round Y Variables
     rounding_var = random.uniform(0, 1)
     rounding_base = 5
 
-    # Process Of Random Rounding
+    # Process Of Random Rounding | Replace With Nan If Sufficiently Small, DEFINED BY Lower_CutOut
     y_2 = []
     for y_var in y_1:
-        if (y_var % rounding_base != 0):
-            if rounding_var >= 0.50:
-                y_new = math.ceil((y_var / rounding_base)) * rounding_base
-            else:
-                y_new = math.floor((y_var / rounding_base)) * rounding_base
+
+        # If Value Smaller Than Lower_CutOut
+        if (y_var <= lower_cutout):
+            y_new = np.nan
+
+        # If Value Greater Than Lower CutOut
+        else:
+            if (y_var % rounding_base != 0):
+                if rounding_var >= 0.50:
+                    y_new = math.ceil((y_var / rounding_base)) * rounding_base
+                else:
+                    y_new = math.floor((y_var / rounding_base)) * rounding_base
 
         y_2.append(y_new)
 
-    y_2 = np.array(y_2)
 
-    y_1 = y_1.reshape(-1, 1)
+    # [3.] Get Average Of List And Replace Nan Values
+    y_2 = np.array(y_2)
     y_2 = y_2.reshape(-1, 1)
 
-    return x, y_1, y_2
+    y_3 = y_2.copy()
+    col_mean = np.nanmean(y_3, axis=0)
+    inds = np.where(np.isnan(y_3))
+    y_3[inds] = np.take(col_mean, inds[1])
+
+    y_1 = y_1.reshape(-1, 1)
+    x_1 = x.reshape(-1, 1)
+
+    # Y_1: Original Data
+    # Y_2: Randomly Rounded Data With Nan
+    # Y_3: Randomly Rounded Data & Filled With Mean
+
+    return x, y_1, y_2, y_3
 
 
 """
@@ -61,8 +82,15 @@ Predict Original Values Based On Randomly Rounded Data
 """
 def predict(x_1, y_2):
 
-    # [1.] Reshape Array
-    x_1 = x_1.reshape(-1, 1)
+    original_x_1 = x_1
+
+    # [1.] Remove Nan Values & Associated Indices
+    inds = np.where(np.isnan(y_2))
+    new_x_1 = np.delete(x_1, inds)
+    new_y_2 = np.delete(y_2, inds)
+
+    x_1 = new_x_1.reshape(-1, 1)
+    y_2 = new_y_2.reshape(-1, 1)
 
     # [2.] Split Data Into Training And Testing Sets
     X_train, X_test, y_train, y_test = train_test_split(x_1, y_2, test_size = 0.25)
@@ -75,9 +103,15 @@ def predict(x_1, y_2):
     m = regr.coef_[0]
     b = regr.intercept_
 
-    y_3 = (x_1 * m) + b
+    y_4 = (original_x_1 * m) + b
+    y_4 = y_4.reshape(-1, 1)
 
-    return y_3
+    # Y_1: Original Data
+    # Y_2: Randomly Rounded Data
+    # Y_3: Randomly Rounded Data & Filled With Mean
+    # Y_4: Data Predicted By Randomly Rounded Data, With Nan Values
+
+    return y_4
 
 
 """
@@ -139,8 +173,8 @@ def export_data(x1, y1, y2, y3):
 # ----------------------------------------------------------------------------------------------------------------------
 if __name__ == "__main__":
 
-    x_1, y_1, y_2 = create_data(1000)
-    y_3 = predict(x_1, y_2)
-    calc_stats(y_1, y_2, y_3)
-    plot_data(x_1, y_1, y_2, y_3)
-    export_data(x_1, y_1, y_2, y_3)
+    x_1, y_1, y_2, y_3 = create_data(1000, 150)
+    y_4 = predict(x_1, y_2)
+    calc_stats(y_1, y_3, y_4)
+    plot_data(x_1, y_1, y_3, y_4)
+    export_data(x_1, y_1, y_3, y_4)
