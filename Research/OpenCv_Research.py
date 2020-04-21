22f871a06e7bb1925e5489f64a01950562acc0d9
renacin <renacin.matadeen@ryerson.ca>
2020-04-21 09:55:20 -0400

    Look Into Filtering Pixel Values, And Overlaying Images

--- Research/OpenCv_Research.py
+++ Research/OpenCv_Research.py
@@ -13,24 +13,34 @@ Notes:
         - Be careful when resizing images, 1/2x Smaller, 2x Bigger
 """
 
+def process_image(image):
+        # Convert To GrayScale | Resize Image, Make 1/32 Version
+        scale_factor = 2
+        im_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        im_small = cv2.resize(im, (0,0), fx= 1/scale_factor, fy= 1/scale_factor)
 
+        # Remove Noise From Image | Must Be Bigger Than 3, & Support A Middle Origin Point | Uneven Numbers Only?
+        blur_kern_size = 5
+        img_blurred = cv2.GaussianBlur(im_small,(blur_kern_size, blur_kern_size), 0)
 
+        # Calculate Edge Detection
+        img_edge = cv2.Laplacian(img_blurred, cv2.CV_64F)
 
+        # Resize Image To Original Dimensions
+        processed_image = cv2.resize(img_edge, (0,0), fx= scale_factor, fy= scale_factor)
 
+        # Return Image
+        return processed_image
 
 # ----------------------------------------------------------------------------------------------------------------------
 if __name__ == "__main__":
 
-    # Open Image As Numpy Array | OpenCV Does This Natively
-    im = cv2.imread(r"C:\Users\renac\Documents\Programming\Python\CanadianCensus_DataCleaning\Research\Images\Image.jpg")
+    # Choose Image To Work On
+    in_path = r"C:\Users\renac\Documents\Programming\Python\CanadianCensus_DataCleaning\Research\Images\Image.jpg"
+    im = cv2.imread(in_path)
 
-    # Resize Image, Make 1/32 Version
-    im_small = cv2.resize(im, (0,0), fx=0.03125, fy=0.03125)
+    # Process Image
+    proc_img = process_image(im)
 
-    # Resize Image, Make 32x Bigger
-    im_big = cv2.resize(im_small, (0,0), fx=32, fy=32)
-
-    cv2.imwrite(r"C:\Users\renac\Documents\Programming\Python\CanadianCensus_DataCleaning\Research\Images\Big_Image.jpg", im_big)
-
-
-    # image_ = im[:, :, 0]
+    # Write Image
+    cv2.imwrite(r"C:\Users\renac\Documents\Programming\Python\CanadianCensus_DataCleaning\Research\Images\Processed_Image.jpg", proc_img)
